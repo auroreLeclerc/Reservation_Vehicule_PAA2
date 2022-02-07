1160079c863ef3a846491a830acd6a3f9f19feb0
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-02-07 10:25:32 +0100

    ajout gitignore, reprise des tp TCP pour server/client

---
+++ Client.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    final static int port = 8532;
+    final static int taille = 1024;
+    final static byte buffer[] = new byte[taille];
+    public static void main(String argv[]) throws Exception {
+        InetAddress serveur = InetAddress.getByName(argv[0]);
+        int length = argv[1].length();
+        byte buffer[] = argv[1].getBytes();
+        DatagramPacket dataSent = new DatagramPacket(buffer, length, serveur, port);
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(dataSent);
+        DatagramPacket dataReceived = new DatagramPacket(new byte[taille], taille);
+        socket.receive(dataReceived);
+        System.out.println("Data received : " + new String(dataReceived.getData()));
+        System.out.println("From : " + dataReceived.getAddress() + ":" + dataReceived.getPort());
+    }
+}
---
+++ Server.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.Calendar;
+
+class Server {
+    final static int port = 8532;
+    final static int taille = 1024;
+    final static byte buffer[] = new byte[taille];
+    public static void main(String argv[]) throws Exception {
+        DatagramSocket socket = new DatagramSocket(port);
+        while(true) {
+            DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+            socket.receive(data);
+            System.out.println(data.getAddress());
+            byte newBuffer[] = new byte[taille];
+            Calendar calendar = Calendar.getInstance();
+            String received = new String(data.getData(), 0, data.getLength());
+            newBuffer=(
+                "RECEIVED FROM "+data.getAddress()+" : "+received
+                +"\nTime is "+calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE)
+            ).getBytes();
+            data.setLength(newBuffer.length);
+            data.setData(newBuffer);
+            socket.send(data);
+        }
+    }
+}
