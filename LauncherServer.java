e2789dc299a43232a6eafdee6b35620fd2771d60
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-02-07 12:22:08 +0100

    Sabrina est allé mangé, externalisation de code

---
+++ LauncherServer.java
@@ -0,0 +1,6 @@
+public class LauncherServer {
+    public static void main(String argv[]) {
+        Server server = new Server();
+        server.run();
+    }
+}
--- Server.java
+++ Server.java
@@ -1,37 +1,50 @@
+import java.io.IOException;
 import java.net.*;
 
-class Server {
+class Server extends java.lang.Thread {
     final static int port = 8532;
     final static int taille = 1024;
     final static byte buffer[] = new byte[taille];
-    
-    public static void main(String argv[]) throws Exception {
-        try (DatagramSocket socket = new DatagramSocket(port)) {
-            System.out.println("Server started on "+socket.getLocalAddress());
-            while(true) {
-                DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+    private DatagramSocket socket;
+
+    public void run() {
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Server started on "+socket.getLocalAddress());
+        while(true) {
+            DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+            try {
                 socket.receive(data);
-                String received = new String(data.getData(), 0, data.getLength());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String received = new String(data.getData(), 0, data.getLength());
 
-                System.out.println(data.getAddress()+" requested "+received);
+            System.out.println(data.getAddress()+" requested "+received);
 
-                String toBeSend;
+            String toBeSend;
 
-                switch (received) {
-                    case "voitures":
-                        toBeSend="Listes des voitures et tout";
-                    break;
-                
-                    default:
-                        toBeSend="404 Not Found";
-                    break;
-                }
+            switch (received) {
+                case "voitures":
+                    toBeSend="Listes des voitures et tout";
+                break;
+            
+                default:
+                    toBeSend="404 Not Found";
+                break;
+            }
 
-                byte newBuffer[] = new byte[taille];
-                newBuffer=(toBeSend).getBytes();
-                data.setLength(newBuffer.length);
-                data.setData(newBuffer);
+            byte newBuffer[] = new byte[taille];
+            newBuffer=(toBeSend).getBytes();
+            data.setLength(newBuffer.length);
+            data.setData(newBuffer);
+            try {
                 socket.send(data);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
