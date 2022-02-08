b851948f2e613f1c67508d01e33a585f009a6f26
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-02-08 14:19:50 +0100

    Launcher

---
+++ client/Launcher.java
@@ -0,0 +1,14 @@
+package client;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String argv[]) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        Client client = new Client("127.0.0.1");
+        client.query("voitures");
+        String message = scan.nextLine();
+        client.query(message);
+        scan.close();
+    }
+}
